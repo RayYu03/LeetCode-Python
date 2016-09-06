86ff73b23c3dd478dc78142f9805f2b7e1ead630
rayyu <rayyu03@163.com>
2016-09-06 21:13:32 +0800

    3a

--- Linked List/demo.py
+++ Linked List/demo.py
@@ -1,23 +1,32 @@
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
-
-    def __repr__(self):
-        if self:
-            return "{} -> {}".format(self.val,repr(self.next))
+# Time:  O(n)
+# Space: O(1)
+#
+# Remove all elements from a linked list of integers that have value val.
+#
+# Example
+# Given: 1 --> 2 --> 6 --> 3 --> 4 --> 5 --> 6, val = 6
+# Return: 1 --> 2 --> 3 --> 4 --> 5
+#
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
 class Solution:
-    def reverseList(self,head):
-        reverse = ListNode(float("-inf"))
-        while head:
-            reverse.next, head.next, head = head, reverse.next, head.next
-        return reverse.next
+    # @param {ListNode} head
+    # @param {integer} val
+    # @return {ListNode}
+    def removeElements(self, head, val):
+        dummy = ListNode(float("-inf"))
+        dummy.next = head
 
-if __name__ == "__main__":
-    head = ListNode(1)
-    head.next = ListNode(2)
-    head.next.next = ListNode(3)
-    head.next.next.next = ListNode(4)
-    head.next.next.next.next = ListNode(5)
-    print Solution().reverseList(head)
+        prev, curr = dummy, dummy.next
+
+        while curr:
+            if curr.val == val:
+                prev.next = curr.next
+            else:
+                prev = curr
+            curr = curr.next
+        return dummy.next
