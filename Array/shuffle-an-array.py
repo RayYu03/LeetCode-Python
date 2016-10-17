89fe6ff938cd113e19d34b6380cf5d7c3a901b26
rayyu <rayyu03@163.com>
2016-10-17 17:51:43 +0800

    shuffle-an-array

---
+++ Array/shuffle-an-array.py
@@ -0,0 +1,58 @@
+# Time:  O(n)
+# Space: O(n)
+
+# Shuffle a set of numbers without duplicates.
+#
+# Example:
+#
+# // Init an array with set 1, 2, and 3.
+# int[] nums = {1,2,3};
+# Solution solution = new Solution(nums);
+#
+# // Shuffle the array [1,2,3] and return its result.
+# Any permutation of [1,2,3] must equally likely to be returned.
+# solution.shuffle();
+#
+# // Resets the array back to its original configuration [1,2,3].
+# solution.reset();
+#
+# // Returns the random shuffling of array [1,2,3].
+# solution.shuffle();
+
+import random
+
+class Solution(object):
+
+    def __init__(self, nums):
+        """
+
+        :type nums: List[int]
+        :type size: int
+        """
+        self.__nums = nums
+
+    def reset(self):
+        """
+        Resets the array to its original configuration and return it.
+        :rtype: List[int]
+        """
+        return self.__nums
+
+    def shuffle(self):
+        """
+        Returns a random shuffling of the array.
+        :rtype: List[int]
+        """
+        nums = list(self.__nums)
+        for i in xrange(len(nums)):
+            j = random.randint(i, len(nums) - 1)
+            nums[i], nums[j] = nums[j], nums[i]
+        return nums
+
+if __name__ == '__main__':
+    nums = [1, 2, 3, 4, 5]
+    obj = Solution(nums)
+    print 'Init nums: ',nums
+    print 'After shuffle: ',obj.shuffle()
+    print 'Reset: ',obj.reset()
+    print 'Shuffle again: ',obj.shuffle()
