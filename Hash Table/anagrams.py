cfaa27e6e7ccbada3b4b30d27472e5befea1f28e
rayyu <rayyu03@163.com>
2016-11-18 14:47:25 +0000

    group anagrams

---
+++ Hash Table/anagrams.py
@@ -0,0 +1,28 @@
+# Time:  O(n * glogg), g is the max size of groups.
+# Space: O(n)
+#
+# Given an array of strings, return all groups of strings that are anagrams.
+#
+# Note: All inputs will be in lower-case.
+#
+from collections import defaultdict
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        anagrams_map, result = defaultdict(list), list()
+
+        for s in strs:
+            sorted_str = ("").join(sorted(s))
+            anagrams_map[sorted_str].append(s)
+
+        for anagram in anagrams_map.values():
+            result.append(anagram)
+        return result
+
+if __name__ == "__main__":
+    result = Solution().groupAnagrams(["cat", "dog", "act", "mac"])
+    print result
